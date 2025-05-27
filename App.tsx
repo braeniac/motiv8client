740938c040781ba557bae3b1c88cb0e4b1d2468b
Maninder Singh <manny.singh.live@gmail.com>
2025-05-27 10:25:44 -0400

    basic file setup, basic redux setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { Provider } from "react-redux";
+
+
+
 import "./global.css";
 
 export default function App() {
   return (
     <View>
       <StatusBar style="light" />
-      
+
     </View>
   );
 }
